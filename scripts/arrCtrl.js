27a6976c8de3a1f49f27f80e3d146cd56065d0ca
Gulshan Mishra <egmox2@gmail.com>
2018-01-03 19:52:40 +0530

    First angular app
    My first hello world app in angularjs

---
+++ scripts/arrCtrl.js
@@ -0,0 +1,15 @@
+var appname = angular.module('ngCribs2', []);
+appname.controller('controller2', ['scope', function($scope){
+
+	$scope.arr[
+    	{
+    		"name": "gulshan"
+    		"age": "24"
+    	},
+    	{
+    		"name": "asdgf"
+    		"age": "30"
+    	}
+    ];
+
+}]);
---
+++ scripts/cribsController.js
@@ -0,0 +1,9 @@
+// Code goes here
+
+var appname = angular.module('ngCribs', []);
+appname.controller('cribsController', ['$scope',
+  function($scope) {
+    $scope.greeting = { text: 'Hello' };
+
+    
+}]);
